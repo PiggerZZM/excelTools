61521f39eecc22c53e4a127827ff63a614b66436
PiggerZZM <442823793@qq.com>
2020-10-12 11:11:02 +0800

    修复unmerge_tool单元格格式不为文本的情况

--- src/transform_tools/read_and_write.py
+++ src/transform_tools/read_and_write.py
@@ -30,24 +30,23 @@ def read_and_write(params: dict) -> xlwt.Workbook():
     count = 0
     for row in range(data_row_begin, data_row_end + 1):
         for col in range(data_col_begin, data_col_end + 1):
+            data = str(sheet.cell_value(row, col)).strip()
+            count += 1
+            for top in range(data_row_begin):
+                new_sheet.write(new_row, top, sheet.cell_value(top, col).strip())
 
-            if type(sheet.cell_value(row, col)) is str:
-                count += 1
-                for top in range(data_row_begin):
-                    new_sheet.write(new_row, top, str(sheet.cell_value(top, col)).strip())
+            for left in range(data_col_begin):
+                new_sheet.write(new_row, left + top_attrs, sheet.cell_value(row, left).strip())
 
-                for left in range(data_col_begin):
-                    new_sheet.write(new_row, left + top_attrs, str(sheet.cell_value(row, left)).strip())
+            # 写入额外信息
+            # write_extra(new_sheet, new_row, top_attrs, left_attrs, extra)
 
-                # 写入额外信息
-                # write_extra(new_sheet, new_row, top_attrs, left_attrs, extra)
+            # 写入数值
+            new_sheet.write(new_row, top_attrs + left_attrs, data)
 
-                # 写入数值
-                new_sheet.write(new_row, top_attrs + left_attrs, str(sheet.cell_value(row, col)).strip())
+            logging.info("数据[{}, {}]写入成功".format(row, int_to_upper_char(col)))
 
-                logging.info("数据[{}, {}]写入成功".format(row, int_to_upper_char(col)))
-
-                new_row += 1
+            new_row += 1
 
     logging.info("数据集有效数据个数：{}".format(count))
 
