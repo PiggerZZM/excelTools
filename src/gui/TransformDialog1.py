79ab63f79b4f9f99b0da905962b24f0cab802f8e
PiggerZZM <442823793@qq.com>
2021-04-26 17:32:02 +0800

    Transform1工具UI

--- src/gui/TransformDialog1.py
+++ src/gui/TransformDialog1.py
@@ -4,6 +4,7 @@ from PySide2.QtCore import Slot
 from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import QDialog, QLineEdit, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
 
+from src.gui.UI.Ui_TransformDialog1 import Ui_TransformDialog1
 from src.transform_tools.transform_tool1 import str_to_int, TransformTool1
 from src.utils.exist_util import ExistUtil
 from src.utils.excel_loader import ExcelLoader
@@ -20,38 +21,12 @@ class TransformDialog1(QDialog):
         self.father_window = father_window
         self.success_window = success_window
 
-        self.data_row_begin = QLineEdit()
-        self.data_row_end = QLineEdit()
-        self.data_col_begin = QLineEdit()
-        self.data_col_end = QLineEdit()
-
-        self.data_row_begin_text = QLabel("数据集起始行号：")
-        self.data_row_end_text = QLabel("数据集结束行号：")
-        self.data_col_begin_text = QLabel("数据集起始列号：")
-        self.data_col_end_text = QLabel("数据集结束列号：")
-
-        self.button = QPushButton("确认")
-        self.button.clicked.connect(self.button_clicked)
-
-        # 布局
-        layout = QVBoxLayout()
-        up_layout = QHBoxLayout()
-        down_layout = QHBoxLayout()
-        up_layout.addWidget(self.data_row_begin_text)
-        up_layout.addWidget(self.data_row_begin)
-        up_layout.addWidget(self.data_row_end_text)
-        up_layout.addWidget(self.data_row_end)
-        down_layout.addWidget(self.data_col_begin_text)
-        down_layout.addWidget(self.data_col_begin)
-        down_layout.addWidget(self.data_col_end_text)
-        down_layout.addWidget(self.data_col_end)
-        layout.addLayout(up_layout)
-        layout.addLayout(down_layout)
-        layout.addWidget(self.button)
-        self.setLayout(layout)
+        # 应用UI
+        self.ui = Ui_TransformDialog1()
+        self.ui.setupUi(self)
 
     @Slot()
-    def button_clicked(self):
+    def on_okButton_clicked(self):
         filename = self.father_window.widget.file_path.text()
         sheetname = self.father_window.widget.sheet_name_box.currentText()
         if ExistUtil.check_exists(filename, sheetname):
@@ -59,10 +34,10 @@ class TransformDialog1(QDialog):
             workbook, worksheet = excel_loader.load_excel()
             logging.info("读取{}成功！".format(filename))
             self.father_window.widget.show_text("读取{}成功！".format(filename))
-            data_row_begin = int(self.data_row_begin.text())
-            data_row_end = int(self.data_row_end.text())
-            data_col_begin = str_to_int(self.data_col_begin.text())
-            data_col_end = str_to_int(self.data_col_end.text())
+            data_row_begin = int(self.ui.dataRowBegin.text())
+            data_row_end = int(self.ui.dataRowEnd.text())
+            data_col_begin = str_to_int(self.ui.dataColBegin.text())
+            data_col_end = str_to_int(self.ui.dataColEnd.text())
             transformTool1 = TransformTool1(workbook, worksheet, data_row_begin, data_row_end, data_col_begin,
                                             data_col_end)
             new_workbook = transformTool1.excete()
---
+++ src/gui/UI/Ui_TransformDialog1.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'TransformDialog1.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_TransformDialog1(object):
+    def setupUi(self, TransformDialog1):
+        if not TransformDialog1.objectName():
+            TransformDialog1.setObjectName(u"TransformDialog1")
+        TransformDialog1.resize(546, 248)
+        self.verticalLayoutWidget = QWidget(TransformDialog1)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(30, 20, 481, 211))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.dataRowBeginText = QLabel(self.verticalLayoutWidget)
+        self.dataRowBeginText.setObjectName(u"dataRowBeginText")
+
+        self.horizontalLayout_2.addWidget(self.dataRowBeginText)
+
+        self.dataRowBegin = QLineEdit(self.verticalLayoutWidget)
+        self.dataRowBegin.setObjectName(u"dataRowBegin")
+
+        self.horizontalLayout_2.addWidget(self.dataRowBegin)
+
+        self.dataRowEndText = QLabel(self.verticalLayoutWidget)
+        self.dataRowEndText.setObjectName(u"dataRowEndText")
+
+        self.horizontalLayout_2.addWidget(self.dataRowEndText)
+
+        self.dataRowEnd = QLineEdit(self.verticalLayoutWidget)
+        self.dataRowEnd.setObjectName(u"dataRowEnd")
+
+        self.horizontalLayout_2.addWidget(self.dataRowEnd)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.dataColBeginText = QLabel(self.verticalLayoutWidget)
+        self.dataColBeginText.setObjectName(u"dataColBeginText")
+
+        self.horizontalLayout.addWidget(self.dataColBeginText)
+
+        self.dataColBegin = QLineEdit(self.verticalLayoutWidget)
+        self.dataColBegin.setObjectName(u"dataColBegin")
+
+        self.horizontalLayout.addWidget(self.dataColBegin)
+
+        self.dataColEndText = QLabel(self.verticalLayoutWidget)
+        self.dataColEndText.setObjectName(u"dataColEndText")
+
+        self.horizontalLayout.addWidget(self.dataColEndText)
+
+        self.dataColEnd = QLineEdit(self.verticalLayoutWidget)
+        self.dataColEnd.setObjectName(u"dataColEnd")
+
+        self.horizontalLayout.addWidget(self.dataColEnd)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.okButton = QPushButton(self.verticalLayoutWidget)
+        self.okButton.setObjectName(u"okButton")
+
+        self.verticalLayout.addWidget(self.okButton)
+
+
+        self.retranslateUi(TransformDialog1)
+
+        QMetaObject.connectSlotsByName(TransformDialog1)
+    # setupUi
+
+    def retranslateUi(self, TransformDialog1):
+        TransformDialog1.setWindowTitle(QCoreApplication.translate("TransformDialog1", u"\u884c\u5217\u8f6c\u7f6e", None))
+        self.dataRowBeginText.setText(QCoreApplication.translate("TransformDialog1", u"\u6570\u636e\u96c6\u8d77\u59cb\u884c\u6570\uff1a", None))
+        self.dataRowEndText.setText(QCoreApplication.translate("TransformDialog1", u"\u6570\u636e\u96c6\u7ed3\u675f\u884c\u6570\uff1a", None))
+        self.dataColBeginText.setText(QCoreApplication.translate("TransformDialog1", u"\u6570\u636e\u96c6\u8d77\u59cb\u5217\u6570\uff1a", None))
+        self.dataColEndText.setText(QCoreApplication.translate("TransformDialog1", u"\u6570\u636e\u96c6\u7ed3\u675f\u5217\u6570\uff1a", None))
+        self.okButton.setText(QCoreApplication.translate("TransformDialog1", u"\u786e\u8ba4", None))
+    # retranslateUi
+
