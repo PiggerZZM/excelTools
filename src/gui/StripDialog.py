b3418fea2bc1fcdeae7d44dc978e59e7a385b27d
PiggerZZM <442823793@qq.com>
2021-04-26 16:26:04 +0800

    Strip工具UI

--- src/gui/StripDialog.py
+++ src/gui/StripDialog.py
@@ -4,6 +4,7 @@ from PySide2.QtCore import Slot
 from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import QDialog, QLineEdit, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
 
+from src.gui.UI.Ui_StripDialog import Ui_StripDialog
 from src.utils.exist_util import ExistUtil
 from src.utils.excel_loader import ExcelLoader
 from src.utils.strip_util import StripUtil
@@ -14,27 +15,15 @@ class StripDialog(QDialog):
     def __init__(self, father_window, success_window):
         super().__init__()
         self.setWindowIcon(QIcon("./gui/ZZM.ico"))
-        self.setWindowTitle("去除字符")
         self.father_window = father_window
         self.success_window = success_window
 
-        self.strip_ch = QLineEdit()
-        self.strip_ch_text = QLabel("要去除的字符：")
-
-        self.button = QPushButton("确认")
-        self.button.clicked.connect(self.button_clicked)
-
-        # 布局
-        layout = QVBoxLayout()
-        up_layout = QHBoxLayout()
-        up_layout.addWidget(self.strip_ch_text)
-        up_layout.addWidget(self.strip_ch)
-        layout.addLayout(up_layout)
-        layout.addWidget(self.button)
-        self.setLayout(layout)
+        # 应用UI
+        self.ui = Ui_StripDialog()
+        self.ui.setupUi(self)
 
     @Slot()
-    def button_clicked(self):
+    def on_OKButton_clicked(self):
         filename = self.father_window.widget.file_path.text()
         sheetname = self.father_window.widget.sheet_name_box.currentText()
         if ExistUtil.check_exists(filename, sheetname):
@@ -42,7 +31,7 @@ class StripDialog(QDialog):
             workbook, _ = excel_loader.load_excel()
             logging.info("读取{}成功！".format(filename))
             self.father_window.widget.show_text("读取{}成功！".format(filename))
-            letter = self.strip_ch.text()
+            letter = self.ui.StripChText.text()
             new_workbook = StripUtil.strip(workbook, letter)
             new_workbook.save(filename.split('/')[-1].replace(".xlsx", "_" + sheetname + "_stripped.xlsx"))
             logging.info("去除{}成功！".format(letter))
---
+++ src/gui/UI/Ui_StripDialog.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'StripDialog.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_StripDialog(object):
+    def setupUi(self, StripDialog):
+        if not StripDialog.objectName():
+            StripDialog.setObjectName(u"StripDialog")
+        StripDialog.resize(342, 139)
+        self.verticalLayoutWidget = QWidget(StripDialog)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(40, 10, 266, 121))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.StripChText = QLabel(self.verticalLayoutWidget)
+        self.StripChText.setObjectName(u"StripChText")
+
+        self.horizontalLayout.addWidget(self.StripChText)
+
+        self.StripCh = QLineEdit(self.verticalLayoutWidget)
+        self.StripCh.setObjectName(u"StripCh")
+
+        self.horizontalLayout.addWidget(self.StripCh)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.OKButton = QPushButton(self.verticalLayoutWidget)
+        self.OKButton.setObjectName(u"OKButton")
+
+        self.verticalLayout.addWidget(self.OKButton)
+
+
+        self.retranslateUi(StripDialog)
+
+        QMetaObject.connectSlotsByName(StripDialog)
+    # setupUi
+
+    def retranslateUi(self, StripDialog):
+        StripDialog.setWindowTitle(QCoreApplication.translate("StripDialog", u"\u53bb\u9664\u5b57\u7b26", None))
+        self.StripChText.setText(QCoreApplication.translate("StripDialog", u"\u8981\u53bb\u9664\u7684\u5b57\u7b26\uff1a", None))
+        self.OKButton.setText(QCoreApplication.translate("StripDialog", u"\u786e\u8ba4", None))
+    # retranslateUi
+
